ab9ef3307dd95e69df4549f19c651590d045757c
liaozhihua <liaozhihua@DESKTOP-C1MK9QI>
2020-03-25 21:44:05 +0800

    this is new 2020325 add Wrapper

---
+++ src/com/lyz/filter/EncodingFilter.java
@@ -0,0 +1,65 @@
+package com.lyz.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+
+import com.lyz.wrapper.RequestEncodingWrapper;
+@WebFilter(
+		description = "ecodingFilter",
+		filterName ="encodingFilter",
+		urlPatterns = {"/*"},
+		initParams = {
+			@WebInitParam(name="ENCODING",value="UTF-8")					
+		}
+		)
+public class EncodingFilter implements Filter{
+
+	String encoding="";
+	String filterName="";
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("filter过滤");
+		/*arg0.setCharacterEncoding(encoding);
+		arg1.setCharacterEncoding(encoding);
+		HttpServletRequest req=(HttpServletRequest) arg0;
+		arg2.doFilter(req, arg1);*/
+		HttpServletRequest req=(HttpServletRequest) arg0;
+		if(req.getMethod().equals("GET")) {
+			new RequestEncodingWrapper(req,encoding);
+		}else {
+			req.setCharacterEncoding(encoding);
+		}
+		arg1.setCharacterEncoding(encoding);
+		arg2.doFilter(req, arg1);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("filter初始化");
+		encoding=arg0.getInitParameter("ENCODING");
+		filterName=arg0.getFilterName();
+		if(encoding!=null&&encoding.equals("")) {
+			encoding="UTF-8";
+		}
+		System.out.println("filter过滤编码"+encoding);
+	}
+
+}
---
+++ src/com/lyz/wrapper/RequestEncodingWrapper.java
@@ -0,0 +1,35 @@
+package com.lyz.wrapper;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class RequestEncodingWrapper extends  HttpServletRequestWrapper {
+
+	private String encoding="";
+	public RequestEncodingWrapper(HttpServletRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+	public RequestEncodingWrapper(HttpServletRequest request,String encoding) {
+		super(request);
+		this.encoding=encoding;
+		// TODO Auto-generated constructor stub
+	}
+	public String getParameter(String name) {
+		String value =getRequest().getParameter(name);
+		if(value!=null&&value.equals("")) {
+			try {
+				value=new String(value.trim().getBytes("ISO-8859-1"),encoding);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return value;
+		
+		
+	}
+
+
+}
